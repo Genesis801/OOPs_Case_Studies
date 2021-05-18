6cebcfe1c8f9ae814e401d9c1c91ae44540561b7
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-05-18 17:45:01 +0530

    Create Person.java

---
+++ PersonStudentStaff/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+	public String name;
+	public String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString()
+	{
+		return "Person: ["+this.name+"] Address: ["+this.address+"]";
+	}
+	
+	
+}
