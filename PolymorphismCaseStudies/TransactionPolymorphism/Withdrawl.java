fa72bebb79d70525d4ca913456569ea8df90ed14
Debajyoti Roy <44739430+Genesis801@users.noreply.github.com>
2021-05-19 17:59:56 +0530

    Create Withdrawl.java

---
+++ PolymorphismCaseStudies/TransactionPolymorphism/Withdrawl.java
@@ -0,0 +1,44 @@
+public class Withdrawal extends Transaction{
+	
+	double amount;
+	
+	/**
+	 * 
+	 */
+	public Withdrawal() {
+		super();
+	}
+
+	
+	/**
+	 * @param amount
+	 */
+	public Withdrawal(int accountNum,double amount) {
+		super(accountNum);
+		this.amount = amount;
+	}
+
+	
+	/**
+	 * @return the amount
+	 */
+	public Double getAmount() {
+		return amount;
+	}
+
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+
+	public void execute(double balance) {
+		super.execute();
+		balance=balance-amount;
+		System.out.println("Balance : "+balance);
+	}
+
+}
